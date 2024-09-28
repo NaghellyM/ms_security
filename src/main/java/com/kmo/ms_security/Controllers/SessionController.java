6b15a8796a0a86771bf598dafb8b2d00f33968ae
NaghellyM <naghellymoreno0610@gmail.com>
2024-09-28 17:29:09 -0500

    style: add comment

--- src/main/java/com/kmo/ms_security/Controllers/SessionController.java
+++ src/main/java/com/kmo/ms_security/Controllers/SessionController.java
@@ -14,7 +14,7 @@ import java.util.List;
 @RestController
 @RequestMapping("/sessions")
 public class SessionController {
-
+    // sobre escribir metodos del padre
     @Autowired
     private SessionRepository theSessionRepository;
 
